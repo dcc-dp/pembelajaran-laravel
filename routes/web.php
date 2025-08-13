b569118396aaac77944b10635cf3d01a34070564
rahmanhia <rahmanhiarmdhni@gmail.com>
2025-08-13 19:57:21 +0800

    add:DashboardController

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware(['auth','verified'])->get('/dashboard',[DashboardController::class,
-//     'index'])->name('dashboard');
+Route::middleware(['auth','verified'])->get('/dashboard',[DashboardController::class,
+    'index'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
